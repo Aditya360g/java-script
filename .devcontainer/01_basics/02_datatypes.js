70895c2bcafde8395f6e1f7894abdf89b4fcd692
Aditya sharma <adityasharma101302@gmail.com>
2026-01-21 09:45:23 +0000

    container

---
+++ .devcontainer/01_basics/02_datatypes.js
@@ -0,0 +1,23 @@
+"use strict"; // treat all js code as newer version
+
+//alert(3+3) // we are using node.js not browers
+
+console.log(3+3);
+console.log("Aditya");
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true / false
+// null => standalone valuse
+// undefined =>
+// symbol => unique
+
+// object 
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+console.log(typeof "Aditya");
+
+
+
