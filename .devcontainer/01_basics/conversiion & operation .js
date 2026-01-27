b08b589384a1df522c1fe1787633b7ec5044b8ea
Aditya sharma <adityasharma101302@gmail.com>
2026-01-27 13:06:01 +0000

    conversion &  operation

---
+++ .devcontainer/01_basics/conversiion & operation .js
@@ -0,0 +1,19 @@
+let score = 33
+let score1 = "33"
+
+console.log(typeof score);
+console.log(typeof score1);
+
+let valuseInNumber = Number(score)
+console.log(valuseInNumber);
+
+// "33" => 33
+// "33avbs" => nan
+// true => 1; false => 0
+
+
+let isLoggedIn = 1
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+
+
