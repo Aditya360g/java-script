2196dacdcc22fb249342a9b71619aa2eb553d38c
Aditya sharma <adityasharma101302@gmail.com>
2026-01-27 15:04:08 +0000

    operation

--- .devcontainer/01_basics/conversiion & operation .js
+++ .devcontainer/01_basics/conversiion & operation .js
@@ -1,19 +1,62 @@
 let score = 33
 let score1 = "33"
 
-console.log(typeof score);
-console.log(typeof score1);
+//console.log(typeof score);
+//console.log(typeof score1);
 
-let valuseInNumber = Number(score)
-console.log(valuseInNumber);
+//let valuseInNumber = Number(score)
+//console.log(valuseInNumber);
 
 // "33" => 33
 // "33avbs" => nan
 // true => 1; false => 0
 
 
-let isLoggedIn = 1
-let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//let isLoggedIn = 1
+//let booleanIsLoggedIn = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIn);
+
+
+//***************** operations************** */
+let value = 3
+let negvale = 3
+//console.log(-value);
+
+//console.log(2+2);
+//console.log(2-2);
+//console.log(2*2);
+//console.log(2**3);
+//console.log(2+2);
+//console.log((2+2),(2-2),(2*2),(2**3),(2/2),(2%2));
+
+//let str1 = "Aditya"
+//let str2 = "sharma"
+
+// console.log(str1 + str2);
+
+//console.log("1" + 2);
+//console.log(1 + "2");
+//console.log ("1" + 2 +  2)
+//console.log("1" + 2 + "2");
+//console.log(1 + 2 + "2");
+
+
+//console.log(+true);
+//console.log(+"");
+//console.log(+true);
+//console.log(+true);
+
+//let num1, num2, num3
+
+//num1 = num2 = num3 = 2 + 2
+
+//let gamecounter = 100
+//gamecounter++;
+//console.log(gamecounter);
+
+
+
+
+
 
 
