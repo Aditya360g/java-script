44eea785e3013a0be340398b764096043d48f4e8
Aditya sharma <adityasharma101302@gmail.com>
2026-02-14 08:19:42 +0000

    num & math

---
+++ .devcontainer/01_basics/06_num & math.js
@@ -0,0 +1,28 @@
+//const number = 1000
+//console.log(number);
+
+//const balance = new Number(1000)
+//console.log(balance);
+//console.log(balance.toString().length);
+//console.log(balance.toFixed(2));  // this fun use for banking mount and it will give you 2 digit after point
+
+
+//const newnumber = 123.45
+//onsole.log(newnumber.toFixed(1));  // this will give you 1 digit after point
+
+//const newnuber2 = 15.456
+//console.log(newnuber2.toPrecision(3));  
+
+//const newnuber5 = 23.568
+//console.log(newnuber5.toPrecision(6));  
+//const newnuber3 = 2221.895
+//console.log(newnuber3.toPrecision(5));  
+
+//const newnumber4 = 1000000
+//console.log(newnumber4.toLocaleString(`en-IN`));
+
+
+// +++++++++++++Maths++++++++++++++//
+
+
+// baad mai krunga //
