505e4815697827e6d2857744ef9c2dd1bc2c709e
Aditya sharma <adityasharma101302@gmail.com>
2026-02-14 08:56:40 +0000

    Date

---
+++ .devcontainer/01_basics/07_datesjs.js
@@ -0,0 +1,16 @@
+//let myDate = new Date();
+//console.log(myDate);
+
+//console.log(myDate.toDateString());
+//console.log(myDate.toISOString());
+//console.log(myDate.toLocaleTimeString());
+//console.log(myDate.toLocaleString());
+//console.log(typeof myDate);
+
+
+//let newdate = new Date('2020-01-01');
+//console.log(newdate);
+//console.log(newdate.toDateString());
+
+// abhi or read kr na hai //
+
