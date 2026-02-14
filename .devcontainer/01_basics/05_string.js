e990da4ebca3d449b822febbd1a9edeb21b0ce72
Aditya sharma <adityasharma101302@gmail.com>
2026-02-14 06:50:18 +0000

    string.js

---
+++ .devcontainer/01_basics/05_string.js
@@ -0,0 +1,30 @@
+// const name = "aditya"
+//const repoCount = 40
+
+// console.log(`hello my name is ${name} there is my RepoCount ${repoCount} `);
+
+
+//const zone = new String('     adityasharma    ')
+//console.log(zone);
+//console.log(zone.length);
+//console.log(zone.trim());
+//console.log(zone.indexOf('s'));
+//console.log(zone.toLowerCase());
+//console.log(zone.toUpperCase());
+//console.log(zone.trim());
+
+const name = 'adityashrewastabh'
+console.log(name.slice(0,6));
+console.log(name.slice(0,17));
+
+
+
+
+
+
+
+
+
+
+
+
